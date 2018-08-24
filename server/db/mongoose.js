5162f48f9b9cd434db483c5e4d05472a89359c1d
adri <adri.skrtic@gmail.com>
2018-08-24 12:37:43 -0400

    new heroku fix

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-const connectionString = 'abc';// process.env.CONNECTION_STRING || 'mongodb://localhost:27017/test-app'
+const connectionString = process.env.CONNECTION_STRING || 'mongodb://localhost:27017/test-app'
 mongoose.connect(connectionString).then(()=>{
   console.log('connected');
 }).catch((err)=>console.log(`error connecting: ${err}`));
